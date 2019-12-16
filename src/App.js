1c984019d0cbcc339b9726ad350825c8953d0979
Dung Luu <dung.luu07@gmail.com>
2019-12-16 07:19:24 +0700

    create search form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import { Container, Input } from "semantic-ui-react";
+
+import Home from "./home/Home.js";
+import Tatics from "./tactics/Tatics.js";
+import Fitness from "./fitness/Fitness.js";
+import Tips from "./tips/Tips.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container className="container">
+        <BrowserRouter>
+          <div>
+            <ul className="navigation">
+              <li>
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/tatics">Tatics</NavLink>
+              </li>
+              <li>
+                <NavLink to="/fitness">Fitness</NavLink>
+              </li>
+              <li>
+                <NavLink to="/tips">Tips</NavLink>
+              </li>
+              <li className="nav-search">
+                <Input icon="search" placeholder="Search..." />
+              </li>
+            </ul>
+          </div>
+
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/tatics" component={Tatics}></Route>
+            <Route path="/fitness" component={Fitness}></Route>
+            <Route path="/tips" component={Tips}></Route>
+          </Switch>
+        </BrowserRouter>
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ src/home/Home.js
@@ -0,0 +1,66 @@
+import React, { Component, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import axios from "axios";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/articles")
+      .then(response => {
+        console.log(response);
+        this.setState({ articles: response.data });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Container className="container text-inside">
+          <h2>This is Home</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+            elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+            ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+            lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+            viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+            imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+            ultricies nisi.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+            elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
+            ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+            lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+            viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+            imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+            ultricies nisi.
+          </p>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
